6be452f955c6b72546a17715cdcb61abaa6c3af0
Rodrigo Toledo <rodrigotoledo@Rodrigos-MacBook-Pro.local>
2024-10-08 12:59:04 -0300

    Update Gradle and React Native dependencies

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import { QueryClient, QueryClientProvider} from 'react-query';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import axios from 'axios';
 
-const queryClient = new QueryClient()
-axios.defaults.baseURL = 'https://fe4f-45-71-76-107.ngrok-free.app/api';
+const queryClient = new QueryClient();
+axios.defaults.baseURL = 'https://fa4d-45-71-76-107.ngrok-free.app/api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 axios.interceptors.request.use(
-  async (config) => {
+  async config => {
     const authToken = await AsyncStorage.getItem('authToken');
     if (authToken) {
       config.headers.Authorization = `Bearer ${authToken}`;
     }
     return config;
   },
-  (error) => {
+  error => {
     console.log('Error interceptors:', error);
     return Promise.reject(error);
-  }
+  },
 );
 
 const RootComponent = () => (
