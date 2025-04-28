967db83ede0a1afb183fcd8de671c8e4bb8142c6
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-04-28 14:57:59 -0300

    Add initial axios configuration, authentication context, and tailwind CSS setup- Configure axios baseURL and request interceptor for token- Create AuthProvider with signIn, signOut, and user state- Set up Tailwind CSS config with content, presets, and theme extensions

---
+++ axiosConfig.js
@@ -0,0 +1,13 @@
+// src/axiosConfig.js
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+axios.defaults.baseURL = 'https://206e-45-71-76-136.ngrok-free.app';
+axios.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('authToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axios;
