8bf84012d1f4583818c6ac1c7f891805711a6dfd
RToledo Dev <rodrigo@rtoledo.inf.br>
2025-06-09 20:17:13 -0300

    Update .gitignore, .tool-versions, and add VSCode settings for Ruby and update axiosConfig URL

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,7 @@
 // src/axiosConfig.js
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-axios.defaults.baseURL = 'https://330e-45-71-76-136.ngrok-free.app';
+axios.defaults.baseURL = 'https://eabd-45-70-34-105.ngrok-free.app';
 axios.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem('authToken');
   if (token) {
